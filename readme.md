e18793d94400db3361c0910db78031cdfccc1757
Dagothig <guillaumenoelmartel@gmail.com>
2019-05-01 17:58:01 -0400

    Init

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Age of Empires-style taunt messages support for IRSSI.
+
+Spams you with pleasant noise when people spam loving messages such as 14 over and over.
+
+## Setup
+
+* Setup a taunts folder in a location of your choosing (accessible by IRSSI). Files should be formatted in the same fashion as Age of Empires': number followed by a space followed by the remainder of the file name, for instance "02 No.mp3".
+* Install the taunts script.
+* Set the taunts_dir setting to the proper location.
---
+++ taunts.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/irssi
+#
+# Age of Empires-style taunt messages support.
+
+use strict;
+use vars qw($VERSION %IRSSI);
+$VERSION = '0.00';
+%IRSSI = (
+    authors => 'dagothig',
+    name => 'taunts',
+    description => 'Age of Empires-style taunt messages support.',
+    license => 'Public Domain'
+);
+
+use Irssi;
+
+my @dir_files = ();
+my $dir_path = -1;
+
+sub check_taunts_list {
+    my $new_dir_path = Irssi::settings_get_str("taunts_dir");
+    print $new_dir_path;
+    print $dir_path;
+    if ($new_dir_path eq $dir_path) {
+        return @dir_files;
+    }
+
+    $dir_path = $new_dir_path;
+    if (opendir(my $dir, $dir_path)) {
+        @dir_files = readdir($dir);
+        closedir($dir);
+    } else {
+        print('Cannot load taunts from directory "' . $dir_path . '"');
+        @dir_files = qw();
+    }
+    return @dir_files;
+}
+
+sub taunt {
+    my ($server, $text) = @_;
+    my $taunt = find_taunt($text);
+    system('(play ' . '"' . $taunt . '" > /dev/null 2>&1) &') if defined($taunt);
+}
+
+sub find_taunt {
+    my ($text) = @_;
+    if ($text =~ /[1-9][0-9]*/) {
+        my $pattern = '^0?' . $text . ' .*$';
+        my @taunts = check_taunts_list();
+        my @results = grep(/$pattern/, @taunts);
+        foreach (@results) {
+            return $dir_path . $_;
+        }
+    }
+    return undef;
+}
+
+Irssi::signal_add_last("message public", "taunt");
+Irssi::signal_add_last("message private", "taunt");
+Irssi::signal_add_last("message own_public", "taunt");
+Irssi::signal_add_last("message own_private", "taunt");
+Irssi::settings_add_str("misc", "taunts_dir", "./");
